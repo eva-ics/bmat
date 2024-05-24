ea1150d988ceed5c4edb44b2f5f08dc2c7ea9c05
Serhij S <div@altertech.com>
2024-05-25 00:00:26 +0200

    0.1.22

--- src/hooks/useQueryParams.tsx
+++ src/hooks/useQueryParams.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState, Dispatch } from "react";
  * @returns {string}
  */
 export const encoderBoolean = (v: boolean): string => {
-	return v === true ? "Y" : "";
+  return v === true ? "Y" : "";
 };
 
 /**
@@ -19,7 +19,7 @@ export const encoderBoolean = (v: boolean): string => {
  * @returns {boolean}
  */
 export const decoderBoolean = (v: string): boolean => {
-	return v === "Y";
+  return v === "Y";
 };
 
 /**
@@ -30,7 +30,7 @@ export const decoderBoolean = (v: string): boolean => {
  * @returns {string}
  */
 export const encoderFloat = (v?: number): string => {
-	return v === null || v === undefined ? "" : v.toString();
+  return v === null || v === undefined ? "" : v.toString();
 };
 
 /**
@@ -41,7 +41,7 @@ export const encoderFloat = (v?: number): string => {
  * @returns {number | null}
  */
 export const decoderFloat = (v: string): number | null => {
-	return v === "" ? null : parseFloat(v);
+  return v === "" ? null : parseFloat(v);
 };
 
 /**
@@ -61,25 +61,25 @@ export const encoderInt = encoderFloat;
  * @returns {number | null}
  */
 export const decoderInt = (v: string): number | null => {
-	return v === "" ? null : parseInt(v);
+  return v === "" ? null : parseInt(v);
 };
 
 export interface ComponentData<T> {
-	name: string;
-	value: T;
-	encoder?: (value: T) => any;
-	decoder?: (value: any) => T;
-	setter: Dispatch<T>;
-	pack_json?: boolean;
+  name: string;
+  value: T;
+  encoder?: (value: T) => any;
+  decoder?: (value: any) => T;
+  setter: Dispatch<T>;
+  pack_json?: boolean;
 }
 
 export const useQueryParams = (
-	components: Array<ComponentData<any>>,
-	dependencies?: any
+  components: Array<ComponentData<any>>,
+  dependencies?: any
 ) => {
-	const loaded = useLoadParams(components);
-	useStoreParams(loaded, components, dependencies);
-	return loaded;
+  const loaded = useLoadParams(components);
+  useStoreParams(loaded, components, dependencies);
+  return loaded;
 };
 
 /**
@@ -91,46 +91,49 @@ export const useQueryParams = (
  * @returns {boolean} hook state (are sub-hooks processed or not)
  */
 const useLoadParams = (components: Array<ComponentData<any>>): boolean => {
-	const [loaded, setLoaded] = useState(false);
-	useEffect(() => {
-		const p = new URLSearchParams(document.location.search);
-		for (const component of components) {
-			let data = p.get(component.name);
-			if (data !== undefined && data !== null) {
-				if (component.pack_json) {
-					try {
-						data = JSON.parse(data);
-					} catch (e) {
-						console.warn(`unable to decode qs param ${component.name}: ${e}`);
-					}
-				}
-				component.setter(component.decoder ? component.decoder(data) : data);
-			}
-		}
-		setLoaded(true);
-	}, []);
-	return loaded;
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    const p = new URLSearchParams(document.location.search);
+    for (const component of components) {
+      let data = p.get(component.name);
+      if (data !== undefined && data !== null) {
+        if (component.pack_json) {
+          try {
+            data = JSON.parse(data);
+          } catch (e) {
+            console.warn(`unable to decode qs param ${component.name}: ${e}`);
+          }
+        }
+        component.setter(component.decoder ? component.decoder(data) : data);
+      }
+    }
+    setLoaded(true);
+  }, []);
+  return loaded;
 };
 
 const useStoreParams = (
-	loaded: boolean,
-	components: Array<ComponentData<any>>,
-	dependencies?: any
+  loaded: boolean,
+  components: Array<ComponentData<any>>,
+  dependencies?: any
 ) => {
-	useEffect(() => {
-		if (loaded) {
-			const p = new URLSearchParams(document.location.search);
-			for (const component of components) {
-				let data = component.encoder
-					? component.encoder(component.value)
-					: component.value;
-				if (component.pack_json) {
-					data = JSON.stringify(data);
-				}
-				p.set(component.name, data);
-			}
-			const url = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${p}`;
-			window.history.pushState({ path: url }, "", url);
-		}
-	}, dependencies);
+  useEffect(() => {
+    if (loaded) {
+      const p = new URLSearchParams(document.location.search);
+      for (const component of components) {
+        let data = component.encoder
+          ? component.encoder(component.value)
+          : component.value;
+        if (component.pack_json) {
+          data = JSON.stringify(data);
+        }
+        p.set(component.name, data);
+      }
+      const sp = "?" + p.toString();
+      if (sp !== window.location.search) {
+        const url = `${window.location.protocol}//${window.location.host}${window.location.pathname}${sp}`;
+        window.history.pushState({ path: url }, "", url);
+      }
+    }
+  }, dependencies);
 };
