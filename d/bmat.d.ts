5027cdb8fc263acbc2145a8db3e3559c8f5b9057
Serhij S <s@g5.is>
2024-11-13 19:38:40 +0100

    refs

---
+++ d/bmat.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="./dashtable.d.ts" />
+/// <reference path="./dom.d.ts" />
+/// <reference path="./hooks.d.ts" />
+/// <reference path="./log.d.ts" />
+/// <reference path="./numbers.d.ts" />
+/// <reference path="./time.d.ts" />
+/// <reference path="./tools.d.ts" />
